a7cfb36d1046eb6110c0d0b89c7020c759f17e64
Vladimir Skok <skok@vova.io>
2018-10-08 12:32:30 +0300

    chore(ui): add style imports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import './styles/style.scss';
+// Styles
+import './styles/generic/common.scss';
+import './styles/components/board.scss';
+import './styles/components/column.scss';
+import './styles/components/card.scss';
+
+// Scripts
 import './scripts/index';
