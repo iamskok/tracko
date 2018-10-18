926299bc7e44fd98c236c3741b01b20ee7538e1c
Vladimir Skok <skok@vova.io>
2018-10-18 15:56:04 +0300

    fix(ui): make renderer properly output buttons
    Fix Renderer to output left and right buttons
    according to a column their in. Add 'js-' specific
    classes in markup.

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -2,10 +2,10 @@ import _ from 'underscore';
 
 export default class Renderer {
 	render(state) {
-		const boardBody = document.getElementsByClassName('Board-body')[0];
+		const jsBoardBody = document.querySelector('.js-Board-body');
 
-		// Append all columns inside `.Board-body`
-		boardBody.innerHTML = _.template(
+		// Append all columns inside `.js-Board-body`
+		jsBoardBody.innerHTML = _.template(
 			// Loop over every column
 			"<% columns.forEach((column) => { %>" +
 				"<section id='<%= column.id %>' class='Column'>" +
@@ -21,10 +21,25 @@ export default class Renderer {
 								"</div>" +
 								"<h3 class='Card-header'><%= card.title %></h3>" + 
 								"<span class='Card-time'><%= card.time %></span>" +
-								"<div class='Card-buttonContainer'>" +
-									"<button class='Card-button isDisabled'>move left</button>" +
-									"<button class='Card-button'>move right</button>" +
-								"</div>" +
+								// Disable left button for the first column
+								"<% if (column === columns[0]) { %>" +
+									"<div class='Card-buttonContainer'>" +
+										"<button class='Card-button isDisabled js-Card-button-isDisabled'>move left</button>" +
+										"<button class='Card-button'>move right</button>" +
+									"</div>" +
+								// Disable right button for the last column
+								"<% } else if (column === columns[columns.length - 1]) { %>" +
+									"<div class='Card-buttonContainer'>" +
+										"<button class='Card-button'>move left</button>" +
+										"<button class='Card-button isDisabled js-Card-button-isDisabled'>move right</button>" +
+									"</div>" +
+								// Output all buttons for the the rest of the columns
+								"<% } else { %>" +
+									"<div class='Card-buttonContainer'>" +
+										"<button class='Card-button'>move left</button>" +
+										"<button class='Card-button'>move right</button>" +
+									"</div>" +
+								"<% } %>" +
 							"</div>" +
 						"<% }); %>" +
 					"<% } %>" +
