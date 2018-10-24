526cc4e0f8bdeaa76a6ec17fb7763095ed0c5bab
Vladimir Skok <skok@vova.io>
2018-10-24 18:23:15 +0400

    feat(controller): pass IDs to taskMove methods in mainController
    Update MainController.handleClick to pass task IDs in taskMoveLeft and taskMoveRight methods

--- src/scripts/mainController.js
+++ src/scripts/mainController.js
@@ -13,26 +13,27 @@ class MainController {
 		if (event.target.parentElement.className.includes('js-Card-buttonContainer--shift')) {
 			// Check if the button is not disabled
 			if (!event.target.className.includes('js-Card-button-isDisabled')) {
+				// Get `id` of a clicked card 
+				const id = event.target.parentElement.parentElement.id;
 				// Check it it's "move left" button
 				if (event.target === event.target.parentElement.childNodes[0]) {
-					// console.log('move left');
-					this.taskMoveLeft();
+					this.taskMoveLeft(id);
 				}
 				// Check if it's "move right" button
 				if (event.target === event.target.parentElement.childNodes[1]) {
-					this.taskMoveRight();
+					this.taskMoveRight(id);
 				}
 			}
 		}
 		event.stopPropagation();
 	}
 
-	taskMoveLeft() {
-		console.log('move left');
+	taskMoveLeft(id) {
+		console.log('move left', 'id:', id);
 	}
 
-	taskMoveRight() {
-		console.log('move right');
+	taskMoveRight(id) {
+		console.log('move right', 'id:', id);
 	}
 }
 
