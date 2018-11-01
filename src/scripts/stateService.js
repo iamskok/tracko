a78cf1bd699db6abdb09b645143bc21b74a4c393
Vladimir Skok <skok@vova.io>
2018-11-01 16:41:51 +0400

    fix: StateService methods
    Update forEach with for loops in taskMoveRight and taskMoveLeft methods, remove static. Add constructor, setState and getState methods.

--- src/scripts/stateService.js
+++ src/scripts/stateService.js
@@ -1,63 +1,56 @@
-/* REVIEW
-Зачем и тут и в MainCtrl import state?
-Этот класс инициализируется MainCtrl
-потом
-StateService.setState(state)
-* */
 import state from './state';
 
-/* REVIEW
-не тут - стейт хранится в StateService
-* */
-const columns = state.columns;
-
 export default class StateService {
-
-    /* REVIEW
-	эти методы - не статик
-	они должны работать с this.state
-	* */
-
-	static taskMoveLeft(id) {
-		columns.forEach((column, columnIndex) => {
-			if (columns[0] !== column) {
-				if (column.cards) {
-					column.cards.forEach((card, cardIndex) => {
-						if (card.id === id) {
-							if (columns[columnIndex - 1].cards) {
-								// Add card to the target column card collection
-								columns[columnIndex - 1].cards.push(card);
-							} else {
-								// Create target column card collection and add card
-								columns[columnIndex - 1].cards = Array.of();
-								columns[columnIndex - 1].cards.push(card);
-							}
-							// Remove the card from the source column card collecion
-							columns[columnIndex].cards.splice(cardIndex, 1);
-						}
-					});
-				}
-			}
-		});
+	constructor() {
+		this.state = state;
 	}
 
-	static taskMoveRight(id) {
-		for (let i = 0; i < columns.length; i++) {
-			if (columns[columns.length - 1] !== columns[i]) {
-				if (columns[i].cards) {
-					for (let j = 0; j < columns[i].cards.length; j++) {
-						if (columns[i].cards[j].id === id) {
-							if (columns[i + 1].cards) {
-								// Add card to the target column card collection
-								columns[i + 1].cards.push(columns[i].cards[j]);
-							} else {
-								// Create target column card collection and add card
-								columns[i + 1].cards = Array.of();
-								columns[i + 1].cards.push(columns[i].cards[j]);
-							}
-							// Remove the card from the source column card collecion
-							return columns[i].cards.splice(j, 1);
+	setState(state) {
+		this.state = state;
+	}
+
+	getState() {
+		return this.state;
+	}
+
+	taskMoveLeft(id) {
+		for (let i = 0; i < state.columns.length; i++) {
+			const column = state.columns[i];
+			if (state.columns[0] !== column) {
+				if (column.cards) {
+					const cardIndex = column.cards.findIndex(card => card.id === id);
+					const card = cardIndex !== -1 && column.cards[cardIndex];
+					if (card) {
+						const prevColumn = state.columns[i - 1];
+						if (prevColumn.cards) {
+							prevColumn.cards.push(card);
+						} else {
+							prevColumn.cards = [card];
 						}
+
+						return column.cards.splice(cardIndex, 1);
+					}
+				}
+			}
+		}
+	}
+
+	taskMoveRight(id) {
+		for (let i = 0; i < state.columns.length; i++) {
+			const column = state.columns[i];
+			if (state.columns[state.columns.length - 1] !== column) {
+				if (column.cards) {
+					const cardIndex = column.cards.findIndex(card => card.id === id);
+					const card = cardIndex !== -1 && column.cards[cardIndex];
+					if (card) {
+						const nextColumn = state.columns[i + 1];
+						if (nextColumn.cards) {
+							nextColumn.cards.push(card);
+						} else {
+							nextColumn.cards = [card];
+						}
+
+						return column.cards.splice(cardIndex, 1);
 					}
 				}
 			}
