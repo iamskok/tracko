220d484a6d0f5804f6163d6e6087e2dfba355f75
Vladimir Skok <skok@vova.io>
2018-11-01 16:49:50 +0400

    refactor: removeCardAnimation method in Renderer
    Remove state as an argument from removeCardAnimation. Import task template.

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -1,61 +1,20 @@
 import _ from 'underscore';
+import board from './template';
 
 export default class Renderer {
 	render(state) {
 		const boardBody = document.querySelector('.js-Board-body');
-
-		// Append all columns inside `.js-Board-body`
-		boardBody.innerHTML = _.template(
-			// Loop over every column
-			"<% columns.forEach((column) => { %>" +
-				"<section id='<%= column.id %>' class='Column'>" +
-					"<h2 class='Column-header'><%= column.title %></h2>" +
-					// Check if there are any cards in a column
-					"<% if (column.cards !== undefined && column.cards.length > 0) { %>" +
-						// Loop over every card in a column
-						"<% column.cards.forEach((card) => { %>" +
-							"<div id=<%= card.id %> class='Card isAnimated'>" +
-								"<div class='Card-buttonContainer'>" +
-									"<button class='Card-button'>edit</button>" +
-									"<button class='Card-button'>remove</button>" +
-								"</div>" +
-								"<h3 class='Card-header'><%= card.title %></h3>" + 
-								"<span class='Card-time'><%= card.time %></span>" +
-								// Disable left button for the first column
-								"<% if (column === columns[0]) { %>" +
-									"<div class='Card-buttonContainer js-Card-buttonContainer--shift'>" +
-										"<button class='Card-button isDisabled js-Card-button js-isDisabled'>move left</button>" +
-										"<button class='Card-button'>move right</button>" +
-									"</div>" +
-								// Disable right button for the last column
-								"<% } else if (column === columns[columns.length - 1]) { %>" +
-									"<div class='Card-buttonContainer js-Card-buttonContainer--shift'>" +
-										"<button class='Card-button'>move left</button>" +
-										"<button class='Card-button isDisabled js-Card-button js-isDisabled'>move right</button>" +
-									"</div>" +
-								// Output all buttons for the the rest of the columns
-								"<% } else { %>" +
-									"<div class='Card-buttonContainer js-Card-buttonContainer--shift'>" +
-										"<button class='Card-button'>move left</button>" +
-										"<button class='Card-button'>move right</button>" +
-									"</div>" +
-								"<% } %>" +
-							"</div>" +
-						"<% }); %>" +
-					"<% } %>" +
-					// Check if button exists
-					"<% if (column.button) { %>" +
-						"<button class='Column-button'><%= column.button %></button>" +
-					"<% } %>" +
-				"</section>" +
-			"<% }); %>"
-		)(state);
+		boardBody.innerHTML = _.template(board)(state);
 	}
 
-	removeCardAnimation(state, delay = 300) {
+	removeCardAnimation(delay = 300) {
 		const cards = Array.from(document.getElementsByClassName('Card'));
-		setTimeout(() => (cards.forEach((card) => {
-			return card.className = card.className.replace('isAnimated', '');
-		})), delay);
+		if (!delay) {
+			cards.forEach((card) => card.className = card.className.replace('isAnimated', ''));	
+		} else {
+			setTimeout(() => (cards.forEach((card) => {
+				return card.className = card.className.replace('isAnimated', '');
+			})), delay);
+		}
 	}
 }
