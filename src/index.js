f329f405573b19f712f918d188adcf561f8a310f
Vladimir Skok <skok@vova.io>
2018-11-01 16:56:34 +0400

    refactor: init MainController in index.js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import './styles/components/column.scss';
 import './styles/components/card.scss';
 
 // Scripts
-import './scripts/mainController';
+import MainController from './scripts/mainController';
+
+new MainController();
