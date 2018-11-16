b291f3ede6547c9ccc1c9c38f777b294c6cd105f
Vladimir Skok <skok@vova.io>
2018-11-16 17:23:59 +0400

    feat(service): add TaskService.put method

--- src/scripts/TaskService.js
+++ src/scripts/TaskService.js
@@ -4,4 +4,17 @@ export default class TaskService {
 	fetch() {
 		return tasks;
 	}
+
+	edit(id, prop) {
+		const tasks = this.fetch();
+		tasks.cards.forEach((task) => {
+			if (task.id === id) {
+				const taskKeys = Object.keys(task);
+				const propKey = Object.keys(prop)[0];
+				if (taskKeys.includes(propKey)) {
+					Object.assign(task, prop);
+				}
+			}
+		});
+	}
 }
