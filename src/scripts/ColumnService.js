3e6ebe86b01e0587f152f2f4d80bbbeeb95672ac
Vladimir Skok <skok@vova.io>
2018-11-19 16:08:47 +0400

    refactor(service): edit column service to be a singleton

--- src/scripts/ColumnService.js
+++ src/scripts/ColumnService.js
@@ -1,6 +1,14 @@
 import columns from './columns';
 
 export default class ColumnService {
+	constructor() {
+		if (!ColumnService.instance) {
+			ColumnService.instance = this;
+		} else {
+			return this;
+		}
+	}
+
 	fetch() {
 		return columns;
 	}
