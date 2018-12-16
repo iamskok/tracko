dfd024d526b63044dc90e64b1c19ea6aa359cc39
Vladimir Skok <skok@vova.io>
2018-12-16 16:16:42 +0400

    refactor(view): add data atributes
    Add data atributes in template instead of using task and column services for
    MainController.handleTaskMoveClick

--- src/scripts/template.js
+++ src/scripts/template.js
@@ -4,7 +4,11 @@ export default `
 		<h2 class="Column-header"><%= column.title %></h2>
 		<% if (column.cards !== undefined && column.cards.length > 0) { %>
 			<% column.cards.forEach((card) => { %>
-				<div id=<%= card.id %> class="Card isAnimated js-Card">
+				<div
+					id=<%= card.id %>
+					class="Card isAnimated js-Card"
+					data-column-id=<%= card.columnId %>
+				>
 					<div class="Card-buttonContainer">
 						<button class="Card-button">edit</button>
 						<button class="Card-button">remove</button>
@@ -12,30 +16,62 @@ export default `
 					<h3 class="Card-header"><%= card.title %></h3> 
 					<% if (column === columns[0]) { %>
 						<div class="Card-buttonContainer">
-							<button class="Card-button isDisabled js-Card-button--left"
-								 data-id=<%= card.id %>>move left
+							<button
+								class="Card-button isDisabled js-Card-button--left"
+								data-id=<%= card.id %>
+								data-column-id=<%= card.columnId %>
+								data-disabled="true"
+								data-move="left"
+							>
+								move left
 							</button>
 
-							<button class="Card-button js-Card-button--right" 
-								 data-id=<%= card.id %>>move right
+							<button
+								class="Card-button js-Card-button--right"
+								data-id=<%= card.id %>
+								data-column-id=<%= card.columnId %>
+								data-move="right"
+							>
+								move right
 							</button>
 						</div>
 					<% } else if (column === columns[columns.length - 1]) { %>
 						<div class="Card-buttonContainer">
-							<button class="Card-button js-Card-button--left" 
-								 data-id=<%= card.id %>>move left
+							<button
+								class="Card-button js-Card-button--left" 
+								data-id=<%= card.id %>
+								data-column-id=<%= card.columnId %>
+								data-move="left"
+							>
+								move left
 							</button>
-							<button class="Card-button isDisabled js-Card-button--right"
-								 data-id=<%= card.id %>>move right
+							<button
+								class="Card-button isDisabled js-Card-button--right"
+								data-id=<%= card.id %>
+								data-disabled="true"
+								data-column-id=<%= card.columnId %>
+								data-move="right"
+							>
+								move right
 							</button>
 						</div>
 					<% } else { %>
 						<div class="Card-buttonContainer">
-							<button class="Card-button js-Card-button--left"
-								 data-id=<%= card.id %>>move left
+							<button
+								class="Card-button js-Card-button--left"
+								data-id=<%= card.id %>
+								data-column-id=<%= card.columnId %>
+								data-move="left"
+							>
+								move left
 							</button>
-							<button class="Card-button js-Card-button--right" 
-								 data-id=<%= card.id %>>move right
+							<button
+								class="Card-button js-Card-button--right"
+								data-id=<%= card.id %>
+								data-column-id=<%= card.columnId %>
+								data-move="right"
+							>
+								move right
 							</button>
 						</div>
 					<% } %>
