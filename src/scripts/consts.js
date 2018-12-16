9d3cbf2060b71b8843e9cc3300417bcd61da8cf7
Vladimir Skok <skok@vova.io>
2018-12-16 16:22:00 +0400

    refactor: remove unsed class names
    Remove button move left and right class names

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -2,8 +2,6 @@ const selectors = {
 	animated: 'isAnimated',
 	disabled: 'isDisabled',
 	board: 'js-Board-body',
-	btnLeft: 'js-Card-button--left',
-	btnRight: 'js-Card-button--right',
 	card: 'js-Card'
 };
 
